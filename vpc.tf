3fb9dc6c63238068fa95f1284bc4bc003e30bf1c
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-09-25 15:40:50 +0530

    Add two public subnets to VPC

--- vpc.tf
+++ vpc.tf
@@ -2,8 +2,27 @@ resource "aws_vpc" "aws_project" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
-
   tags = {
     Name = "aws-project"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.aws_project.id
+  cidr_block              = var.public_subnet_1_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "aws-project-public-subnet-1"
+    Type = "Public"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.aws_project.id
+  cidr_block              = var.public_subnet_2_cidr
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "aws-project-public-subnet-2"
+    Type = "Public"
   }
 }
