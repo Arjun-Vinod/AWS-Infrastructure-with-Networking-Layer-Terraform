1a28745f30b6f451d4f2d7aac774e1feaa97628c
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-09-25 15:53:10 +0530

    Add two private subnets to VPC

--- vpc.tf
+++ vpc.tf
@@ -28,3 +28,23 @@ resource "aws_subnet" "public_subnet_2" {
     Type = "Public"
   }
 }
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.aws_project.id
+  cidr_block        = var.private_subnet_1_cidr
+  availability_zone = data.aws_availability_zones.available.names[0]
+  tags = {
+    Name = "aws-project-private-subnet-1"
+    Type = "Private"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.aws_project.id
+  cidr_block        = var.private_subnet_2_cidr
+  availability_zone = data.aws_availability_zones.available.names[1]
+  tags = {
+    Name = "aws-project-private-subnet-2"
+    Type = "Private"
+  }
+}
