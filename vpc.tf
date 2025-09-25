ff95524afdd47ea2964b8dd08e6a13563b99ce8b
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-09-25 16:43:37 +0530

    Add route-table-assosiation for the Subnets

--- vpc.tf
+++ vpc.tf
@@ -88,3 +88,23 @@ resource "aws_route_table" "private_rt_2" {
     Name = "aws-project-private-rt-2"
   }
 }
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt_1.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt_2.id
+}
