a9c9bab49eddd19eb2b31cb7883441d092b0cf89
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-09-25 15:44:19 +0530

    Add data to fetch the avilable availability zones

--- providers.tf
+++ providers.tf
@@ -7,9 +7,12 @@ terraform {
   }
 }
 
-# Configure AWS Provider
 provider "aws" {
   region = var.aws_region
   access_key = "<Your Access Key>"
   secret_key = "<Your Secret Key>"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
 }
--- vpc.tf
+++ vpc.tf
@@ -10,6 +10,7 @@ resource "aws_vpc" "aws_project" {
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.aws_project.id
   cidr_block              = var.public_subnet_1_cidr
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
   tags = {
     Name = "aws-project-public-subnet-1"
@@ -20,6 +21,7 @@ resource "aws_subnet" "public_subnet_1" {
 resource "aws_subnet" "public_subnet_2" {
   vpc_id                  = aws_vpc.aws_project.id
   cidr_block              = var.public_subnet_2_cidr
+  availability_zone       = data.aws_availability_zones.available.names[1]
   map_public_ip_on_launch = true
   tags = {
     Name = "aws-project-public-subnet-2"
