6db0a3f2d9c39fa8fbf88934c00144126ec935d8
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-09-25 16:50:48 +0530

    Add security group for bastion host

--- vpc.tf
+++ vpc.tf
@@ -108,3 +108,27 @@ resource "aws_route_table_association" "private_subnet_2_association" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_rt_2.id
 }
+
+resource "aws_security_group" "bastion_sg" {
+  name        = "bastion-security-group"
+  description = "Security group for bastion host"
+  vpc_id      = aws_vpc.aws_project.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "bastion-security-group"
+  }
+}
+
