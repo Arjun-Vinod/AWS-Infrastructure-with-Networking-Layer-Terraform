d6ba08b2244067cc9ba27aaef2a8269e9d7f1800
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-09-25 16:58:59 +0530

    Add security group for Load Balancer

--- vpc.tf
+++ vpc.tf
@@ -158,3 +158,26 @@ resource "aws_security_group" "private_sg" {
     Name = "private-instances-security-group"
   }
 }
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-security-group"
+  description = "Security group for Application Load Balancer"
+  vpc_id      = aws_vpc.aws_project.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "alb-security-group"
+  }
+}
