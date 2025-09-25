34e06aa7f0b8e30b5f1a76d7681a166a6a774151
Arjun-Vinod <arjun.vinodaj77@gmail.com>
2025-09-25 15:25:05 +0530

    Add providers.tf file

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure AWS Provider
+provider "aws" {
+  region = var.aws_region
+  access_key = "<Your Access Key>"
+  secret_key = "<Your Secret Key>"
+}
